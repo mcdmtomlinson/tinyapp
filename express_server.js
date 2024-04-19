26c75b351b3259841376ee26a2e53bd51cb8c988
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-19 16:43:46 -0600

    Update cookie to be for user_id rather than username and updated header to grab the email address associated with this updated cookie

--- express_server.js
+++ express_server.js
@@ -53,7 +53,7 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render('urls_index', templateVars);
 });
@@ -61,14 +61,14 @@ app.get("/urls", (req, res) => {
 //Responds to '/urls/new' GET request with rendered HTML of urls_new.ejs
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/register", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_registration", templateVars);
 })
@@ -78,7 +78,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
 });
