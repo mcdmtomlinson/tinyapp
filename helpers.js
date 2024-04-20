ba12fbc0d53d2f3c667388982060caf4320a8f3f
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-20 03:00:24 -0600

    refactor helper functions to helpers.js and README addition

--- helpers.js
+++ helpers.js
@@ -5,5 +5,20 @@ const getUserByEmail = (email, database) => {
   }
   return false;
 };
+//random 6 string generator
+const generateRandomString = () => Math.random().toString(36).substring(7);
 
-module.exports = { getUserByEmail };
+
+//  function that returns the URLs where the userID is equal to the id of the currently logged in user.
+const urlsForUser = (id, database) => {
+  let urlArr = [];
+  for (let shortURL in database) {
+    if (database[shortURL].userID === id) {
+      urlArr.push(shortURL);
+    }
+  }
+  return urlArr;
+};
+
+
+module.exports = { getUserByEmail, urlsForUser, generateRandomString };
